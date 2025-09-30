712495f4363af6ef7a0e2109668d4cafb260f598
Lucia Korenova <korenova.lucia23@gmail.com>
2025-09-30 14:58:35 +0200

    ts commit

---
+++ src/uloha2.ts
@@ -0,0 +1,26 @@
+function vypisPismena(veta: string): void {
+    // obmedzenie na max 255 znakov
+    const text = veta.slice(0, 255).toLowerCase();
+
+    const pocitadlo: { [key: string]: number } = {};
+
+    for (let i = 0; i < text.length; i++) {
+        const znak = text[i]!;
+
+        // zoberieme len písmená a-z
+        if (znak >= "a" && znak <= "z") {
+            if (!pocitadlo[znak]) {
+                pocitadlo[znak] = 0;
+            }
+            pocitadlo[znak]++;
+        }
+    }
+
+    // pekný výpis
+    for (const pismeno in pocitadlo) {
+        console.log(`${pismeno} ${pocitadlo[pismeno]}`);
+    }
+}
+
+// Príklad:
+vypisPismena("Alabama");
