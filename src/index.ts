e54910b1ec3f18f0011a7a08c085c0ad1c65abe1
Lucia Korenova <korenova.lucia23@gmail.com>
2025-09-30 14:30:27 +0200

    ts commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("ahoj svet!")
+function bmiIndex (weight:number, height:number): string{
+    const heightInMeters:number = height/100;
+    const bmi:number = weight/(heightInMeters*heightInMeters);
+    const healthyWeight: number = 25 * (heightInMeters * heightInMeters);
+    const schudnut:number =  weight - healthyWeight;
+    if (bmi<=19) {
+        return "podváha";}
+    else if (19<bmi && bmi<=25) {
+        return "normálna hmotnosť";}
+    else if (25<bmi && bmi<=30){
+        return `nadváha, schudni ${schudnut.toFixed(1)} kg.`;}
+    else{
+        return `obezita, schudni ${schudnut.toFixed(1)} kg.`;
+    }
+}
+console.log(bmiIndex(70,170));
