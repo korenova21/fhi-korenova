b080a4dcac49578286c62cfe552193236a3b79a3
Lucia Korenova <korenova.lucia23@gmail.com>
2025-10-01 17:40:22 +0200

    ts commit

--- src/uloha4.ts
+++ src/uloha4.ts
@@ -1,3 +1,5 @@
-const vstup = prompt("Zadaj dve čísla, napr. -2 5:")!;
+import readlineSync from "readline-sync";
+
+const vstup = readlineSync.question("Zadaj dve čísla, napr. -2 5: ");
 const [a = 0, n = 0] = vstup.split(" ").map(Number);
 console.log(`${a}^${n} = ${a ** n}`);
